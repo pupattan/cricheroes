8ab142fb6afaf39d64166c7c5e227dfd0f0c28ee
pupattan <pkbsdmp@gmail.com>
2022-12-10 00:11:25 +0530

    initial check in

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cricheroes',
+    version='0.0.1',
+    packages=['cricheroes'],
+    install_requires=[],
+    url='https://github.com/pupattan/cricheroes',
+    license='MIT',
+    author='pattap',
+    author_email='pulak.pattanayak@gmail.com',
+    description='Python APIs to fetch and store team data from cricheroes.in'
+)
